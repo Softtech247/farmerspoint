8565fa5b08d055bef8aa8b189dc5a16dd65119fc
softtech247 <kschuxy@gmail.com>
2023-05-12 09:42:13 +0100

    Footer Modification

--- farmerspoint/settings.py
+++ farmerspoint/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-t7j&r=uuyumla!sgg-j*0*vme_=xaxw+a$3krrnbk2svh@lqr3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
